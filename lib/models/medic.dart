72a68eb6fb2ce75e2017d2a242e5a6bd355393d1
faroukgassara <faroukgasaraa@gmail.com>
2020-06-10 23:15:34 +0100

    .

--- lib/models/medic.dart
+++ lib/models/medic.dart
@@ -1,19 +1,19 @@
 import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 class Medic with ChangeNotifier   {
   String DESIGN;
   String LABO;
+  int PRIX_REMB;
   
 
-  Medic({this.DESIGN,this.LABO});
+  Medic({this.DESIGN,this.LABO,this.PRIX_REMB});
 
   factory Medic.formJson(Map <String, dynamic> json){
     return new Medic(
        DESIGN: json['DESIGN'],
        LABO: json['LABO'],
-       //PRIX_REF:json['PRIX_REF'],
+       PRIX_REMB:json['PRIX_REMB'] as int ,
     );
   }
 }
