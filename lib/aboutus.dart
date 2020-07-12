d1a6693f7426073e42f3bdf2a16792384d4c04a6
faroukgassara <faroukgasaraa@gmail.com>
2020-07-12 15:53:51 +0100

    .

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -1,3 +1,4 @@
+import 'package:final1/modifierprofile.dart';
 import 'package:final1/theme/colors/light_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
@@ -91,6 +92,7 @@ class _AboutUsState extends State<AboutUs> {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -101,71 +103,94 @@ class _AboutUsState extends State<AboutUs> {
           child: SingleChildScrollView(
             child: Stack(
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Column(
-                            children: <Widget>[
-                              CircularPercentIndicator(
-                                radius: 90.0,
-                                lineWidth: 5.0,
-                                animation: true,
-                                percent: 0.75,
-                                circularStrokeCap: CircularStrokeCap.round,
-                                progressColor: LightColors.kRed,
-                                backgroundColor: Colors.blue[200],
-                                center: CircleAvatar(
-                                  backgroundColor: Colors.blue[100],
-                                  radius: 35.0,
-                                  backgroundImage: AssetImage(
-                                    'assets/images/doctor_avatar.png',
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(
+                                20.0, 10.0, 30.0, 10.0),
+                            child: Column(
+                              children: <Widget>[
+                                CircularPercentIndicator(
+                                  radius: 90.0,
+                                  lineWidth: 5.0,
+                                  animation: true,
+                                  percent: 0.75,
+                                  circularStrokeCap: CircularStrokeCap.round,
+                                  progressColor: LightColors.kRed,
+                                  backgroundColor: Colors.blue[200],
+                                  center: CircleAvatar(
+                                    backgroundColor: Colors.blue[100],
+                                    radius: 35.0,
+                                    backgroundImage: AssetImage(
+                                      'assets/images/doctor_avatar.png',
+                                    ),
                                   ),
                                 ),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Text('Dr.' + ' ' + Nom + ' ' + Prenom),
+                              Column(
+                                children: <Widget>[
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ModifierProfile(
+                                                  Code: Code,
+                                                  Prenom: Prenom,
+                                                  Nom: Nom,
+                                                  Adresse: Adresse,
+                                                  Specialite: Specialite,
+                                                  Ville: Ville,
+                                                  CodeP: CodeP,
+                                                  TelBur1: TelBur1,
+                                                  TelBur2: TelBur2,
+                                                  TelDom: TelDom,
+                                                  Gsm: Gsm,
+                                                  Email: Email,
+                                                  Fax: Fax,
+                                                )),
+                                      );
+                                    },
+                                    child: Container(
+                                      height: 30,
+                                      child: Row(
+                                        children: <Widget>[
+                                          Container(
+                                            child: Text(
+                                              'Modifier',
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w700,
+                                                  fontSize: 18),
+                                            ),
+                                            decoration: BoxDecoration(
+                                              color: Colors.blue,
+                                              borderRadius:
+                                                  BorderRadius.circular(30),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ],
                           ),
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Text('Dr.' + ' ' + Nom + ' ' + Prenom),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            CircularPercentIndicator(
-                              radius: 90.0,
-                              lineWidth: 5.0,
-                              animation: true,
-                              percent: 0.75,
-                              circularStrokeCap: CircularStrokeCap.round,
-                              progressColor: LightColors.kRed,
-                              backgroundColor: Colors.blue[200],
-                              center: CircleAvatar(
-                                backgroundColor: Colors.blue[100],
-                                radius: 35.0,
-                                backgroundImage: AssetImage(
-                                  'assets/images/doctor_avatar.png',
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Text(Prenom),
-                            Text(Prenom),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
